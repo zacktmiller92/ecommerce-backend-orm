010f181b741ab69c44b6cd0b722f9fd269e8da5e
zacktmiller92 <zacktmiller92@gmail.com>
2021-09-12 16:35:14 -0400

    added category delete route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -79,7 +79,22 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({ message: 'No category found with this id' });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
