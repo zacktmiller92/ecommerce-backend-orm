f6ef94c6fa17040c383cac784561c1e476df2269
zacktmiller92 <zacktmiller92@gmail.com>
2021-09-12 12:30:35 -0400

    added models

--- config/connection.js
+++ config/connection.js
@@ -1,35 +1,34 @@
-const Sequelize = require('sequelize');
 require('dotenv').config();
 
-let sequelize;
+const Sequelize = require('sequelize');
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
-    port: 3306
+    dialectOptions: {
+      decimalNumbers: true,
+    },
   });
-}
 
 module.exports = sequelize;
 
 
-// require('dotenv').config();
 
 // const Sequelize = require('sequelize');
+// require('dotenv').config();
 
-// const sequelize = process.env.JAWSDB_URL
-//   ? new Sequelize(process.env.JAWSDB_URL)
-//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+// let sequelize;
+
+// if (process.env.JAWSDB_URL) {
+//   sequelize = new Sequelize(process.env.JAWSDB_URL);
+// } else {
+//   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
 //     host: 'localhost',
 //     dialect: 'mysql',
-//     dialectOptions: {
-//       decimalNumbers: true,
-//     },
+//     port: 3306
 //   });
+// }
 
 // module.exports = sequelize;
-
-// import the Sequelize constructor from the library
